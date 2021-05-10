07f863f0211b90809e0bbf94fc7aa338b60c8f8a
Zaheer Shaikh <44533598+travaco@users.noreply.github.com>
2021-05-10 17:05:27 -0500

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HighlightPipe } from './highlight.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HighlightPipe
   ],
   imports: [
     BrowserModule,
